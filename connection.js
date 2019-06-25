5fa5a792bebee8c3be01b8174f71c2277e11ddf0
falihnaufal17 <dudu.kotkot@gmail.com>
2019-06-25 20:00:24 +0700

    connect to mysql db

---
+++ connection.js
@@ -0,0 +1,11 @@
+require('dotenv/config');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+});
+
+module.exports = connection;

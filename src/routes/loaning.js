2bae5e56f9c8e73ff16ced6877e172f0060a4ec8
falihnaufal17 <dudu.kotkot@gmail.com>
2019-07-25 13:33:05 +0700

    get loan must authorization

--- src/routes/loaning.js
+++ src/routes/loaning.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const Route = express.Router()
 const loaningController = require('../controllers/loaning')
-
+const Auth = require('../helpers/auth')
 Route
-    .get('/', loaningController.getLoaning)
+    .get('/', Auth.authInfo, Auth.accessToken, loaningController.getLoaning)
     .post('/', loaningController.addLoaning)
-    .get('/:loaningid', loaningController.detailLoaning)
-    .get('/users/:iduser', loaningController.getLoanByUser)
+    .get('/:loaningid', Auth.authInfo, Auth.accessToken, loaningController.detailLoaning)
+    .get('/users/:iduser', Auth.authInfo, Auth.accessToken, loaningController.getLoanByUser)
     .delete('/:loaningid', loaningController.deleteLoaning)
     .patch('/:loaningid', loaningController.updateLoaning)
 

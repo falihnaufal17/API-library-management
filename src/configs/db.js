b1ce7c558554da1e17452ebab11c1b3bec6bda45
falihnaufal17 <dudu.kotkot@gmail.com>
2019-07-30 19:29:33 +0700

    deploy heroku dude

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,10 +2,10 @@ require('dotenv/config');
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    host: 'remotemysql.com',
+    user: 'eLQnwUDQz3',
+    password: 'nSyJvnbLGm',
+    database: 'eLQnwUDQz3',
 });
 
 module.exports = connection;

b40ac7ab0db0f4d1055bea33c753a5a81d04fa23
falihnaufal17 <dudu.kotkot@gmail.com>
2019-07-31 14:09:53 +0700

    deployy

--- app.js
+++ app.js
@@ -19,41 +19,22 @@ app.listen(port, () => {
     console.log(`Server started with port: ${port}`)
 });
 
-// const corsOptions = (req, callback) => {
-//     if (whitelist.split('').indexOf(req.header('origin')) !== -1) {
-//         console.log('success')
-//         return callback(null, {
-//             origin: true
-//         })
-//     } else {
-//         console.log('Failed')
-//         return callback(null, {
-//             origin: false
-//         })
-//     }
-// }
+const corsOptions = (req, callback) => {
+    if (whitelist.split('').indexOf(req.header('origin')) !== -1) {
+        console.log('success')
+        return callback(null, {
+            origin: true
+        })
+    } else {
+        console.log('Failed')
+        return callback(null, {
+            origin: false
+        })
+    }
+}
 
-// app.use(cors())
-// app.options('*', cors(corsOptions))
-app.use(function (req, res, next) {
-    /*var err = new Error('Not Found');
-     err.status = 404;
-     next(err);*/
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
-
-    //  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
-    // Pass to next layer of middleware
-    next();
-});
+app.use(cors())
+app.options('*', cors(corsOptions))
 app.use(xssFilter())
 app.use(logger('dev'))
 

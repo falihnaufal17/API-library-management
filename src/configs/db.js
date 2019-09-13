ff9a5ac9343daaa3d4c339f0edb54581691ea8bb
= <=>
2019-09-14 07:45:36 +0800

    config db to env

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,10 +2,10 @@ require('dotenv/config');
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST || "remotemysql.com",
-    user: process.env.DB_USER || "eLQnwUDQz3",
-    password: process.env.DB_PASS || "nSyJvnbLGm",
-    database: process.env.DB_NAME || "eLQnwUDQz3",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
 });
 
 module.exports = connection;
